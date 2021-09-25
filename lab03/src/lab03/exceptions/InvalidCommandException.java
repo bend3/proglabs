9174bd1fd7233edaf689f775377979ffa2ccd951
duck <szekelybendeguz7@gmail.com>
2021-09-25 22:53:47 +0200

    lab03 small fixes

--- lab03/src/lab03/exceptions/InvalidCommandException.java
+++ lab03/src/lab03/exceptions/InvalidCommandException.java
@@ -3,7 +3,7 @@ package lab03.exceptions;
 public class InvalidCommandException extends Exception {
     @Override
     public void printStackTrace() {
-        System.out.println(this);
+        System.out.println(toString());
     }
 
     @Override
