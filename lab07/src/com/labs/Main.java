7c1e0c4bc4a672c6b0aa93cd14a053022b1c66b1
duck <szekelybendeguz7@gmail.com>
2021-10-24 18:16:26 +0200

    lab07 main added XD

---
+++ lab07/src/com/labs/Main.java
@@ -0,0 +1,16 @@
+package com.labs;
+
+import com.labs.caesar.CaesarFrame;
+
+import javax.swing.*;
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame jFrame = new CaesarFrame();
+        jFrame.setVisible(true);
+    }
+
+
+}
